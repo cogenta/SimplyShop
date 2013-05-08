66c8cc0b735f854b117b7a58a7d2c89fa10da790
Will Harris <will@greatlibrary.net>
2013-05-08 09:59:35 +0100

    Log best price decisions.

--- SimplyShop/CSPriceContext.m
+++ SimplyShop/CSPriceContext.m
@@ -32,7 +32,9 @@
 - (void)getBestPrice:(id<CSPriceList>)prices
             callback:(void (^)(id<CSPrice>))callback
 {
+    NSLog(@"Best price: choosing from %@ for %@", prices, self);
     if (prices.count <= 0) {
+        NSLog(@"Best price: choosing nil because there are no prices");
         callback(nil);
         return;
     }
@@ -46,31 +48,40 @@
             id<CSPrice> bestPrice = nil;
             BOOL bestPriceLiked = NO;
             for (id<CSPrice> price in priceArray) {
+                NSLog(@"Best price: evaluating %@ against current best %@ (liked = %@)",
+                      price, bestPrice, @(bestPriceLiked));
                 BOOL liked = [likedURLs containsObject:price.retailerURL];
                 if ( ! bestPrice) {
+                    NSLog(@"Best price: new best is %@ (first price seen)", price);
                     bestPrice = price;
                     bestPriceLiked = liked;
                     continue;
                 }
                 
                 if (liked && ! bestPriceLiked) {
+                    NSLog(@"Best price: new best is %@ (first liked seen)", price);
                     bestPrice = price;
                     bestPriceLiked = liked;
                     continue;
                 }
                 
                 if (bestPriceLiked && ! liked) {
+                    NSLog(@"Best price: rejecting %@ (disliked and current liked)", price);
                     continue;
                 }
                 
                 NSComparisonResult comparison = [[bestPrice effectivePrice]
                                                  compare:[price effectivePrice]];
                 if (comparison == NSOrderedDescending) {
+                    NSLog(@"Best price: new best is %@ (better effective price)", price);
                     bestPrice = price;
                     continue;
                 }
+                
+                NSLog(@"Best price: rejecting %@ (not better effective price)", price);
             }
             
+            NSLog(@"Best price: chose %@", bestPrice);
             callback(bestPrice);
         }];
     }];
